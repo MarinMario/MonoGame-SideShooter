0a0f6cf2bfaa293bb5c75e248a1e51319b5a60d8
Marin Mario <38869813+marinmario@users.noreply.github.com>
2020-07-25 00:34:45 +0300

    initialization

--- SideScrollerShooter/Program.cs
+++ SideScrollerShooter/Program.cs
@@ -14,7 +14,7 @@ namespace SideScrollerShooter
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new SideScrollerShooter())
                 game.Run();
         }
     }
--- SideScrollerShooter/SideShooter.cs
+++ SideScrollerShooter/SideShooter.cs
@@ -7,12 +7,12 @@ namespace SideScrollerShooter
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class SideScrollerShooter : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public SideScrollerShooter()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
